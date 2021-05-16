a479c3df5d0e39e689969a134b0353c67ed0faae
LSWarss <ls.warss98@gmail.com>
2021-05-16 20:40:18 +0200

    Initial Commit

---
+++ State-Driven-Navigation/State_Driven_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_Driven_NavigationApp.swift
+//  State-Driven-Navigation
+//
+//  Created by Łukasz Stachnik on 16/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct State_Driven_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
